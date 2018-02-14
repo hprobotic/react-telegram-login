b706ca3abe8f47096d625ac505f48db37377be98
John Pham <hprobotic@gmail.com>
2018-02-14 21:03:37 +0700

    Update index.js

--- examples/index.js
+++ examples/index.js
@@ -2,13 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import TelegramLoginButton from 'react-telegram-login';
 
-const responseTelegram = response => {
+const handleTelegramResponse = response => {
   console.log(response);
 };
-
+ 
 ReactDOM.render(
-  <div>
-    <TelegramLoginButton dataOnauth={this.responseTelegram} botName="OdauBot" />
-  </div>,
-  document.getElementById('telegram-login')
+  <TelegramLoginButton dataOnauth={handleTelegramResponse} botName="OdauBot" />,
+  document.getElementById('telegramButton')
 );
