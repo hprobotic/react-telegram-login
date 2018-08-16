81d774d88cfda6bfa6735013606bfbaf7e4df8ee
loginwashere <login.was.here@gmail.com>
2018-08-16 15:37:02 +0300

    Update widget script to fetch

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ class TelegramLoginButton extends React.Component {
     };
 
     const script = document.createElement('script');
-    script.src = 'https://telegram.org/js/telegram-widget.js?2';
+    script.src = 'https://telegram.org/js/telegram-widget.js?4';
     script.setAttribute('data-telegram-login', botName);
     script.setAttribute('data-size', dataSize);
     if (dataRadius !== undefined) {
